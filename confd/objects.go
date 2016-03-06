b4eeaff3acede0ad3c1d62355176f53ffd0a4780
Vincent Landgraf <Vincent.Landgraf@Sophos.de>
2016-03-06 14:54:39 +0100

    go like license reference

--- confd/objects.go
+++ confd/objects.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Vincent Landgraf. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package confd
 
 // ObjectMeta confd object metadata
