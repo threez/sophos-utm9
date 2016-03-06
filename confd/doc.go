d45a2eda91fa497c3a8cb83577710a40641b18a5
Vincent Landgraf <Vincent.Landgraf@Sophos.de>
2016-03-06 15:15:06 +0100

    updates package description

--- confd/doc.go
+++ confd/doc.go
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package confd implements a confd client connection.
+// Package confd implements a SOPHOS UTM 9.x confd client.
 package confd
